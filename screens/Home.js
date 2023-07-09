f02652a6f337ea13c9debcfbd6131b57821af747
ChetankumarSB <itismechetankumar@gmail.com>
2023-07-09 11:01:34 +0530

    Home ui completed

--- screens/Home.js
+++ screens/Home.js
@@ -1,66 +1,117 @@
-
-import React, { useState} from 'react';
-import {ScrollView, Button , View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
-
-
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, Text, Image, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import DocumentPicker from 'react-native-document-picker';
+import Snackbar from 'react-native-snackbar';
+import RNFS from 'react-native-fs';
 
 const Home = ({ title, author, date, image }) => {
+  const [text, setText] = useState('');
+  const [description, setDescription] = useState('');
+  const [parsedData, setParsedData] = useState([]);
 
-  const [isLoading, setLoading] = useState(true);
-  const [selected, setSelected] = useState('');
+  useEffect(() => {
+    // TODO: Fetch data from the server and update the state
+  }, []);
 
-  const handelbutton = () => {
-    console.log(selected)
-  }
+  const uploadImage = async () => {
+    let base64response;
+    try {
+      base64response = await RNFS.readFile(parsedData[0].uri, 'base64');
+    } catch (error) {
+      console.log('Error:', error);
+    }
 
-  const onSelectSwitch = index => {
+    const imageObj = {
+      name: parsedData[0].name,
+      baseimage: base64response,
+      title: text,
+      description: description,
+    };
 
- if (index==1) {
-  setLoading(true)
-  
- } else {
-  setLoading(false)
- }
+    // TODO: Post the imageObj to the server
+    console.log(imageObj);
+  };
 
+  const imageSelect = async () => {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images],
+      });
+
+      const imageres = JSON.stringify(res);
+      const parsedData = JSON.parse(imageres);
+      setParsedData(parsedData);
+    } catch (err) {
+      setParsedData([]);
+      if (DocumentPicker.isCancel(err)) {
+        Snackbar.show({
+          text: 'File selection canceled',
+          duration: Snackbar.LENGTH_SHORT,
+          backgroundColor: '#242B2E',
+        });
+      } else {
+        Snackbar.show({
+          text: 'Something went wrong',
+          duration: Snackbar.LENGTH_SHORT,
+          backgroundColor: '#D82E2F',
+        });
+        throw err;
+      }
+    }
   };
 
   return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text style={styles.titleText}>Hello, $TeacherUsername</Text>
 
-  <SafeAreaView>
-    <ScrollView>
+        <View style={styles.card}>
+          <View style={styles.content}>
+            <TouchableOpacity onPress={imageSelect}>
+              {parsedData[0]?.uri ? (
+                <Image source={{ uri: parsedData[0].uri }} style={styles.image} />
+              ) : (
+                <Image source={require('../src/copy-upload.jpeg')} style={styles.image} />
+              )}
+            </TouchableOpacity>
 
-    <Text style={styles.titleText}>Hello, $KLEITians</Text>
+            <TextInput label="Title" value={text} onChangeText={text => setText(text)} />
 
+            <TextInput
+              label="Description"
+              multiline
+              numberOfLines={4}
+              value={description}
+              onChangeText={description => setDescription(description)}
+            />
+          </View>
+          <Button icon="upload" mode="contained"  color="#FF0000"  onPress={uploadImage}>
+              Upload
+            </Button>
+        </View>
 
-     <View style={styles.card}>
-      <View style={styles.content}>
-      <Image source={{ uri: 'https://th-i.thgim.com/public/incoming/8bb5nr/article65407753.ece/alternates/FREE_1200/2699_12_5_2022_17_58_4_1_13HUBLIKLEIT.JPG', }} style={styles.image} />
-        <Text style={styles.title}>KLE Institute of Technology{title}</Text>
-        <Text style={styles.author}>10min ago from MCA Department{author}</Text>
-        <Text style={styles.info}>{date}The K.L.E Society's KLE Institute of Technology is an engineering college in Hubli, India. Established in 2008, it is one of the institutes under the banner of Karnatak Lingayat Education Society. KLEIT is approved by the AICTE and recognized by University Grant Commission of India.
-  </Text>
-      </View>
-    </View>
-    <View style={styles.card}>
-      <View style={styles.content}>
-      <Image source={{ uri: 'https://cache.careers360.mobi/media/colleges/social-media/media-gallery/4075/2021/8/5/Campus%20View%20of%20KLE%20Institute%20of%20Technology%20Hubli_Campus-View.jpg', }} style={styles.image} />
-        <Text style={styles.title}>Title{title}</Text>
-        <Text style={styles.author}>10min ago from MCA Department{author}</Text>
-        <Text style={styles.info}>10min ago{date}</Text>
-      </View>
-    </View>
-
-    <View style={styles.card}>
-      <View style={styles.content}>
-      <Image source={{ uri: 'https://reactnative.dev/img/tiny_logo.png', }} style={styles.image} />
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.author}>By {author}</Text>
-        <Text style={styles.date}>{date}</Text>
-      </View>
-    </View>
-   
-
-</ScrollView>
+        <View style={styles.card}>
+          <View style={styles.content}>
+            <Image
+              source={{ uri: 'https://cache.careers360.mobi/media/colleges/social-media/media-gallery/4075/2021/8/5/Campus%20View%20of%20KLE%20Institute%20of%20Technology%20Hubli_Campus-View.jpg' }}
+              style={styles.image}
+            />
+            <Text style={styles.title}>Title{title}</Text>
+            <Text style={styles.info}>10min ago{date}</Text>
+          </View>
+                    <Button
+            icon="delete"
+            mode="contained"
+            onPress={uploadImage}
+            style={{ backgroundColor: '#D82E2F' }}
+          >
+            Delete
+          </Button>
+          
+        </View>
+        
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -90,10 +141,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-  author: {
-    marginTop: 5,
-    fontSize: 16,
-  },
   info: {
     marginTop: 5,
     fontSize: 14,
@@ -101,11 +148,11 @@ const styles = StyleSheet.create({
   },
   titleText: {
     fontSize: 25,
-    fontWeight: "bold",
-    color:"#242B2E",
-    marginTop:15,
-    marginLeft:20,
-    marginBottom:10,
+    fontWeight: 'bold',
+    color: '#242B2E',
+    marginTop: 15,
+    marginLeft: 20,
+    marginBottom: 10,
   },
 });
 
--- screens/Timetable.js
+++ screens/Timetable.js
@@ -1,128 +1,97 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text, TextInput } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
 
-import CustomTimePicker from './CustomTimepicker'; // Import the CustomTimePicker component
+const classdata = [
+  { label: 'Kannada', value: '1' },
+  { label: 'English', value: '2' },
+  { label: 'Hindi', value: '3' },
+  { label: 'Science', value: '4' },
+  { label: 'Social science', value: '5' },
+  { label: 'Mathematics', value: '6' },
+  { label: 'Sports', value: '7' },
+  { label: 'Drawing', value: '8' },
+];
 
-const Timetable = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const [time1, setTime1] = useState('');
-  const [time2, setTime2] = useState('');
-  const [className, setClassName] = useState('');
+const DropdownComponent = () => {
+  const [value, setValue] = useState(null);
 
-  const navigation = useNavigation();
-
-  const handleNextPage = () => {
-    navigation.navigate('NextPage');
-  };
-
-  const toggleSwitch = () => {
-    setIsEnabled((previousState) => !previousState);
+  const renderItem = item => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.textItem}>{item.label}</Text>
+      </View>
+    );
   };
 
   return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.tableRow}>
-          <Text style={styles.weekHeader}>   SUN</Text>
-          <Text style={styles.weekHeader}>   MON</Text>
-          <Text style={styles.weekHeader}>    TUE</Text>
-          <Text style={styles.weekHeader}>   WED</Text>
-          <Text style={styles.weekHeader}>   THU</Text>
-          <Text style={styles.weekHeader}>    FRI</Text>
-          <Text style={styles.weekHeader}>    SAT</Text>
-        </View>
+    <Dropdown
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      inputSearchStyle={styles.inputSearchStyle}
+      iconStyle={styles.iconStyle}
+      data={classdata}
+      search
+      maxHeight={300}
+      labelField="label"
+      valueField="value"
+      placeholder="Select class"
+      searchPlaceholder="Search..."
+      value={value}
+      onChange={item => {
+        setValue(item.value);
+      }}
 
-        <View style={styles.tableRow}>
-          <Text style={styles.tableHeader}> Time </Text>
-          <Text style={styles.tableHeader}> Monday </Text>
-        </View>
-
-        <View style={styles.tablecolumn}>
-          <View style={styles.tablecolumn}>
-            <CustomTimePicker value={time1} onChange={(time) => setTime1(time)} />
-          </View>
-          <View style={styles.tablecolumn}>
-            <CustomTimePicker value={time2} onChange={(time) => setTime2(time)} />
-          </View>
-        </View>
-
-        <View style={styles.tableRow}>
-          <Text style={styles.tableHeader}> Class </Text>
-        </View>
-
-        <View style={styles.tableRow}>
-          <TextInput
-            style={styles.input}
-            value={className}
-            onChangeText={(text) => setClassName(text)}
-            placeholder="Enter class name"
-          />
-        </View>
-
-        {/* Repeat the above pattern for other rows */}
-      </View>
-    </>
+      renderItem={renderItem}
+    />
   );
 };
 
+export default DropdownComponent;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#FFFFFF',
+  dropdown: {
+    margin: 16,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 12,
+    padding: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
   },
-  tableRow: {
+  icon: {
+    marginRight: 5,
+  },
+  item: {
+    padding: 17,
     flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: '#CCCCCC',
-    paddingVertical: 5,
-  },
-  tablecolumn: {
-    flex: 0,
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: '#CCCCCC',
-    paddingVertical: 5,
-  },
-  weekHeader: {
-    flex: 1,
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  tableHeader: {
-    flex: 1,
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'left',
-  },
-  tableCell: {
-    flex: 1,
-    fontSize: 14,
-    textAlign: 'center',
-  },
-  input: {
-    flex: 1,
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#CCCCCC',
-    borderRadius: 5,
-    paddingHorizontal: 6,
-    marginLeft: 5,
-  },
-  button: {
-    marginTop: 10,
-    backgroundColor: '#DDDDDD',
-    padding: 6,
-    borderRadius: 5,
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
-  buttonText: {
+  textItem: {
+    flex: 1,
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#555555',
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
   },
 });
-
-export default Timetable;
